fb8363c746ee31a4130cd9d5762a6a81a85aae00
Austin Wood <austin@austinkwood.com>
2017-02-26 11:56:26 -0800

    Read each line of each file in /corpus

--- char_counter.rb
+++ char_counter.rb
@@ -1,4 +1,11 @@
 file_extension = "rb"
 Dir.glob("corpus/**/*.#{file_extension}") do |file|
   puts "working on: #{file}"
+
+  line_num = 0
+  text = File.open(file).read
+  text.gsub!(/\r\n?/, "\n")
+  text.each_line do |line|
+    print "#{line_num += 1} #{line}"
+  end
 end

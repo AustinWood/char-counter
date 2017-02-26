34091c4fa456599168f5e99792d1f4215f38a0e6
Austin Wood <austin@austinkwood.com>
2017-02-26 12:25:29 -0800

    Create separate alphabet hash for each language in the corpus

--- char_counter.rb
+++ char_counter.rb
@@ -1,24 +1,25 @@
-file_extension = "rb"
+file_extensions = %w(swift rb)
+file_extensions.each do |ext|
 
-alphabet = {}
-alphabet.default = 0
+  alphabet = {}
+  alphabet.default = 0
 
-Dir.glob("corpus/**/*.#{file_extension}") do |file|
+  Dir.glob("corpus/**/*.#{ext}") do |file|
 
-  puts "working on: #{file}"
+    puts "working on: #{file}"
 
-  line_num = 0
-  text = File.open(file).read
-  text.gsub!(/\r\n?/, "\n")
-  text.each_line do |line|
-    print "#{line_num += 1} #{line}"
+    line_num = 0
+    text = File.open(file).read
+    text.gsub!(/\r\n?/, "\n")
+    text.each_line do |line|
+      # print "#{line_num += 1} #{line}"
+      letters = line.downcase.chars
+      letters.each do |letter|
+        alphabet[letter] +=1
+      end
 
-    letters = line.downcase.chars
-    letters.each do |letter|
-      alphabet[letter] +=1
     end
-
   end
-end
 
-p alphabet
+  p alphabet
+end

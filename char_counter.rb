fe8927d944e161fe3f965c7a1a26fc0c0b6dd2ad
Austin Wood <austin@austinkwood.com>
2017-02-26 12:12:57 -0800

    Create a print hash of all characters in the corpus and the frequence with which they appear

--- char_counter.rb
+++ char_counter.rb
@@ -1,5 +1,10 @@
 file_extension = "rb"
+
+alphabet = {}
+alphabet.default = 0
+
 Dir.glob("corpus/**/*.#{file_extension}") do |file|
+
   puts "working on: #{file}"
 
   line_num = 0
@@ -7,5 +12,13 @@ Dir.glob("corpus/**/*.#{file_extension}") do |file|
   text.gsub!(/\r\n?/, "\n")
   text.each_line do |line|
     print "#{line_num += 1} #{line}"
+
+    letters = line.downcase.chars
+    letters.each do |letter|
+      alphabet[letter] +=1
+    end
+
   end
 end
+
+p alphabet

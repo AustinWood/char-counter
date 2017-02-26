6efb34688b68c15f6cf01f09edf657498e6f7c6b
Austin Wood <austin@austinkwood.com>
2017-02-26 12:29:53 -0800

    Cleanup code and modify output format

--- char_counter.rb
+++ char_counter.rb
@@ -5,21 +5,21 @@ file_extensions.each do |ext|
   alphabet.default = 0
 
   Dir.glob("corpus/**/*.#{ext}") do |file|
-
-    puts "working on: #{file}"
-
+    # puts "working on: #{file}"
     line_num = 0
     text = File.open(file).read
     text.gsub!(/\r\n?/, "\n")
     text.each_line do |line|
       # print "#{line_num += 1} #{line}"
-      letters = line.downcase.chars
+      # letters = line.downcase.chars
+      letters = line.chars
       letters.each do |letter|
-        alphabet[letter] +=1
+        alphabet[letter] += 1
       end
-
     end
   end
 
+  puts "File extension: #{ext}"
   p alphabet
+  puts "\n"
 end
